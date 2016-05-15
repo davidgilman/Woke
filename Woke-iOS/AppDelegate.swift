2296b47eab2bd39d22dd64fcdea682ce3e82fae1
davidgilman <daviidgilman@gmail.com>
2016-05-15 02:58:49 -0700

    added in correct api key

--- Woke-iOS/AppDelegate.swift
+++ Woke-iOS/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyDQcahBcQNLg3Xhhm4PGE3jTRxNqExPxLc")
+        GMSServices.provideAPIKey("AIzaSyCt-Kbl_KtU1EL8irbWrySzd3wYfwcFbEE")
         // Override point for customization after application launch.
         return true
     }
