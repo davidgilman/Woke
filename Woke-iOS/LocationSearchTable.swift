8bb2f11628a677d11a5e4f76c35de3eb06af8bab
davidgilman <daviidgilman@gmail.com>
2016-05-20 11:20:23 -0700

    search for location added

--- Woke-iOS/LocationSearchTable.swift
+++ Woke-iOS/LocationSearchTable.swift
@@ -13,7 +13,31 @@ class LocationSearchTable : UITableViewController {
     var matchingItems:[MKMapItem] = []
     var mapView: MKMapView? = nil
     
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        return addressLine
+    }
 }
+
 extension LocationSearchTable : UISearchResultsUpdating {
     func updateSearchResultsForSearchController(searchController: UISearchController) {
         guard let mapView = mapView,
@@ -30,17 +54,18 @@ extension LocationSearchTable : UISearchResultsUpdating {
             self.tableView.reloadData()
         }
     }
-    extension LocationSearchTable {
-        override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return matchingItems.count
-        }
-        
-        override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
-            let selectedItem = matchingItems[indexPath.row].placemark
-            cell.textLabel?.text = selectedItem.name
-            cell.detailTextLabel?.text = ""
-            return cell
-        }
+}
+
+extension LocationSearchTable {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = parseAddress(selectedItem)
+        return cell
     }
 }
